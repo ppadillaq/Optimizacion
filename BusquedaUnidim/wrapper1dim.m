79c27fd667713f414ff683bcc0ad2aa8dfb99e6d
LAPTOP-TIIM87P9\pedro <pedro_padilla_q@hotmail.com>
2024-01-15 18:58:33 +0000

    Comenta función wrapper1dim.

--- BusquedaUnidim/wrapper1dim.m
+++ BusquedaUnidim/wrapper1dim.m
@@ -1,6 +1,17 @@
-function out = wrapper1dim(originalFunction, lambda, X0, S)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-X = X0 + lambda * S;
-out = originalFunction(X);
+function out = wrapper1dim(funcionOriginal, lambda, X0, S)
+    %WRAPPER1DIM Función envoltorio para emplear métodos de búsqueda
+    %   unidimensional en problemas de optimización multidimensional.
+    %   Realiza un cambio de variable en la función objetivo. Sustituye el
+    %   vector de variables de decisión X por una variable escalar lambda,
+    %   que representa el paso a optimizar en la dirección de descenso S.
+    %
+    %   Inputs:
+    %     funcionOriginal: función objetivo original (multidimensional)
+    %     X0: punto inicial
+    %     S: dirección de descenso
+    %     lambda: paso en la dirección S (variable de decisión)
+    
+    X = X0 + lambda * S;
+    out = funcionOriginal(X);
+
 end
