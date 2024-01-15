10462e8041717806dad480e58fe571ccbc5e9c4f
LAPTOP-TIIM87P9\pedro <pedro_padilla_q@hotmail.com>
2024-01-15 16:48:30 +0000

    Adapta la llamada de la función con los nuevos argumentos.

--- ejemplo.m
+++ ejemplo.m
@@ -11,8 +11,15 @@ hold on
 
 % Plot optimum discretized curve that minimizes the revolution surface area
 f = @integral;
-U0 = ones(1,5);%Para 5 (n<10) y 1000 no converge
-[U_sD,S_sD,fX_sD,i_sD] = steepestDescent(f, U0, 'oneDimSearch', "dichoSearch");
+U0 = ones(1,5);
+[U_sD,S_sD,fX_sD,i_sD] = steepestDescent(f, U0, 'maxStep', 0.1, 'oneDimSearch', "dichoSearch");
 x = linspace(0,1,numel(U0));
-scatter(x, U_sD(i_sD,:))
+scatter(x, U_sD(i_sD-1,:))
+ylim([0, 1])
+
+U0 = ones(1,100);
+[U_sD,S_sD,fX_sD,i_sD] = steepestDescent(f, U0, 'maxStep', 10, 'oneDimSearch', "dichoSearch");
+x = linspace(0,1,numel(U0));
+figure
+scatter(x, U_sD(i_sD-1,:))
 ylim([0, 1])
